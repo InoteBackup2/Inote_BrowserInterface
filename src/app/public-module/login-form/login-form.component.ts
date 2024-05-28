d33beec086a5d31b7b35a446997fca18074c6ace
Atsuhiko Mochizuki <atsuhiko_mochizuki27@laposte.net>
2024-05-28 17:08:20 +0200

    Updating login form in progress

--- src/app/public-module/login-form/login-form.component.ts
+++ src/app/public-module/login-form/login-form.component.ts
@@ -1,10 +1,11 @@
-import { SignInResponseDto } from './../shared-public-module/dto/sign-in-response.dto';
-import { SignInRequestDto } from './../shared-public-module/dto/sign-in-request.dto';
-import { Component /*, OnInit*/ } from "@angular/core";
+import { SignInResponseDto } from "./../shared-public-module/dto/sign-in-response.dto";
+import { SignInRequestDto } from "./../shared-public-module/dto/sign-in-request.dto";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { PublicUserService } from "../public-user.service";
 import { Router } from "@angular/router";
 import { catchError, throwError } from "rxjs";
 import { TokenService } from "../../core-module/token.service";
+import { ChangePasswordRequestDto } from "../shared-public-module/dto/change-password-request.dto";
 
 @Component({
   selector: "app-login-form-component",
@@ -15,40 +16,105 @@ import { TokenService } from "../../core-module/token.service";
   ],
 })
 export class LoginFormComponent {
+  @ViewChild("scanModalContent") modalContent: ElementRef | undefined;
+  activationcode!: number;
+  activation_success!: boolean;
+  activationResponseStatus!: number;
+  activationResponseMsgToDisplay!: string;
+  registerResponseMsgToDisplay!: string;
+  receivedActivationCode: any;
+  onSubmitActivation() {
+    throw new Error("Method not implemented.");
+  }
+  // RELATING TEMPLATE VARIABLES
+  // ==============================================
   email!: string;
   password!: string;
   statusAfterRequest!: number;
   msgAfterRequest!: string;
+  askedChangePassword: boolean = false;
+  isModalOpen = false;
+  receveidCode: boolean = false;
+
+  // HTTP
+  // ==============================================
+  /* Request */
+  changePasswordRequestBody!: ChangePasswordRequestDto;
+
+  /* Response */
   signInResponseDto!: SignInResponseDto;
 
+  changePasswordResponseBody!: string | null;
+  changePasswordResponseStatus!: number;
+  changePasswordResponseMsgToDisplay!: string | null;
+  changePassword_success: boolean = false;
+
+  // DEPENDENCIES INJECTIONS BY CONSTRUCTOR
+  // ==============================================
   constructor(
     private publicUserservice: PublicUserService,
     private tokenService: TokenService,
     private router: Router
   ) {}
 
+  // INITIALIZATION (by ngOnInit)
+  // ==============================================
+
+  // TEMPLATE CALLBACKS METHODS
+  // ==============================================
   OnSubmit() {
     const signInRequestDto: SignInRequestDto = {
-      "username": this.email,
-      "password": this.password
+      username: this.email,
+      password: this.password,
     };
 
     this.publicUserservice
       .loginUser(signInRequestDto)
       .pipe(
         catchError((error) => {
-          this.statusAfterRequest = error.status;
-          this.msgAfterRequest = error.error.detail;
+          this.changePasswordResponseStatus = error.status;
+          this.changePasswordResponseBody = error.error.detail;
           return throwError(error);
         })
       )
       .subscribe((response) => {
-        this.statusAfterRequest = response.status;
+        this.changePasswordResponseStatus = response.status;
         if (response.status === 200 && response.body !== null) {
           this.signInResponseDto = response.body;
           this.tokenService.saveToken(this.signInResponseDto.bearer);
           this.router.navigate(["dashboard"]);
         }
       });
+  }
+
+  onChangePasswordAsked() {
+    this.changePasswordRequestBody = {
+      email: this.email,
+    };
+
+    this.publicUserservice
+      .askChangePassword(this.changePasswordRequestBody)
+      .pipe(
+        catchError((error) => {
+          this.changePasswordResponseStatus = error.status;
+          this.changePasswordResponseBody = error.error.detail;
+          return throwError(error);
+        })
+      )
+      .subscribe((response) => {
+        this.askedChangePassword = true;
+        this.changePasswordResponseStatus = response.status;
+        if (response.status === 200 && response.body !== null) {
+          this.changePassword_success = true;
+        } else {
+          throw new Error(
+            "HTTP Response anomaly : body is empty or status code not attempted"
+          );
+        }
+      });
+  }
+
+  onAskingReceiveCode() {
+    this.receivedActivationCode = true;
   }
 }
