6bdb0ed7508295bc6167632d7a4f0f1e398d7767
FYHenry <frederic_yann.henry@protonmail.com>
2024-05-29 19:24:31 +0200

    Added spec files

--- src/app/protected-module/dashboard/dashboard.component.spec.ts
+++ src/app/protected-module/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule/*, HttpTestingController*/ } from '@angular/common/http/testing';
+//import { HttpClient } from '@angular/common/http';
 import { DashboardComponent } from './dashboard.component';
 import { ProtectedNavComponent } from '../shared-protected-module/protected-nav/protected-nav.component';
 import { ProtectedUserService } from '../shared-protected-module/user-module/protected-user.service';
@@ -8,8 +8,8 @@ import { ProtectedUserService } from '../shared-protected-module/user-module/pro
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  //let httpClient: HttpClient;
+  //let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
